e2d68f38d61024c99aa5dcb5aa0c94c391a3edde
RogersCarvalho <48721911+RogersCarvalho@users.noreply.github.com>
2019-04-15 13:54:55 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,24 +1,152 @@
+
 import socket
+import os 
+import datetime as dt
+import threading as th
+import sys
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+def requestMessage (con,cliente,extensionDict,shareFolder):
+    
+    print ("Aguardando requisição...")
+    mensagem = con.recv(1048576).decode('utf-8') #Recebe as informações em bites
 
-HOST = '127.0.0.1'
-PORT = 5000
+    msg = mensagem.split("\n")
+    request['operation'] = msg[0]
+    
+    del(msg[0])
+    
+    print(request['operation'])
 
-print('Iniciado serviço ...')
+    #debug variable
+    cont = 0
 
-try:
-    tcp.bind((HOST, PORT))
-    tcp.listen(1) #número de cliente que pode lidar por vez
-except socket.error as e:
-    print(str(e))
+    for line in msg:
+        cont = cont+1
+        print ('linha...',line)
+        lineSplit = line.split(': ')
+        try:
+            key = lineSplit[0]
+            valor = lineSplit[1]
+            request[key] = valor
+        except:
+            break
+
+    try:
+        filepath = request['operation'].split()[1]
+    except:
+        filepath='servConfig/400.html'
+        
+    if filepath == '/':
+        nameFile = request['operation'].split()
+
+        file = open(shareFolder + '/Index.html','rb')
+
+        fileByte = file.read()
+
+        respostaString = '\nHTTP/1.1 200 Ok \r\n' #traz o index
+
+        resposta = {
+            "Location" : "http://localhost:7000/",
+            'date' : str(dt.datetime.now()),
+            'Server' : 'server',
+            'Content-Type' : 'text/html',
+            'Content-Lenght' : str(len(fileByte))
+
+        }
+        for key,valor in resposta.items():
+            respostaString = respostaString + key+': '+ valor + '\r\n'
+
+        respostaString = respostaString + '\r\n'
+        con.send( respostaString.encode('utf-8') + fileByte )
+
+    else:
+            
+        if os.path.isfile(shareFolder + filepath) :
+            file = open(shareFolder + filepath,'rb')
+            respostaString = '\nHTTP/1.1 200 okk! \r\n' #arquivos
+            fileByte = file.read()
+            index = filepath.rfind('.')
+            keyExtension = filepath[index:]
+            
+        elif os.path.isdir(shareFolder + filepath):
+            
+            files = os.listdir(shareFolder + filepath)
+            createListHtml(filepath,files,shareFolder)
+            
+            keyExtension = '.isdir'
+            
+            file = open(shareFolder+'/temp/listDir.html','rb')
+            fileByte = file.read()
+            respostaString = '\nHTTP/1.1 200 okkk! \n'
+            
+            
+        else:
+            file = open('servConfig/404.html','rb')
+            respostaString = '\nHTTP/1.1 404 Not Found! \r\n'
+            fileByte = file.read()
+            keyExtension = '.html'
+
+        resposta = {
+            "Location" : "http://localhost:7000/",
+            'date' : str(dt.datetime.now()),
+            'Server' : 'server',
+            'Content-Type' : extensionDict[keyExtension],
+            'Content-Length' : str(len(fileByte))
+
+        }
+
+        for key,valor in resposta.items():
+            respostaString = respostaString + key+': '+ valor + '\r\n'
+        
+        respostaString = respostaString + '\r\n'
+        file.close()
+        con.sendall( respostaString.encode('utf-8') + fileByte )
+    con.close()
 
 
-while True:
-    con, cliente = tcp.accept() #aceita requisções e guarda a conexão e IP do cliente
-    print('Conectado por', cliente) #informa o ip do cliente que se conectou
-    while True:
-        dados = con.recv(1024) #recebe e armazena a mensagem do cliente
-        if not dados: break
-        con.send(b'......:'+ dados) #retorna o dado enviado pelo cliente
-con.close()
+def createListHtml(filePath,files,shareFolder):
+    file = open(shareFolder + '/temp/listDir.html','w')
+    file.write('<html>')
+    file.write('<head><title>listDir</title></head>')
+    file.write('<body>')
+    file.write('<h1>MUTHERFUCKER PAGES</H1>')
+    for fileName in files:
+        file.write('<a href="' + filePath + '/'+fileName+'">'+fileName+'</a><br>')
+
+    file.write('</body>')
+    file.write('</html>')
+    file.close()
+    
+request = {}
+host = 'localhost'#host = '0.0.0.0'
+port = int(sys.argv[1])#port = 8080
+
+shareFolder = sys.argv[2]
+loadextensions = open('servConfig/extension.txt','r')
+extensionDict = {}
+
+for line in loadextensions:
+    keyValue = line.split('\t')
+    index = keyValue[1].find('\r\n')
+    extensionDict[keyValue[0]] = keyValue[1][:index]
+    
+loadextensions.close()
+
+
+addr = (host, port)
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.bind(addr) #vicula o ip a porta
+serv_socket.listen(10) #número de clientes por vez
+
+
+file = ''
+fileByte = ''
+cons = set()
+cont = 0
+while True:    
+    con, cliente = serv_socket.accept() ###Escutando
+    print ('conectado...')
+    cons.add(con)
+    th.Thread(target=requestMessage,args=(con, cliente, extensionDict,shareFolder)).start()
+    
