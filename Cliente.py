b094fe4669059b0c717ba2cb49de1e5c0c3b37ee
RogersCarvalho <48721911+RogersCarvalho@users.noreply.github.com>
2019-04-15 13:57:49 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,33 +1,112 @@
 import socket
 
-serverHOST = str(input('Host_Servidor:'))
-#serverPORT = int(input('porta:'))
+url = input('url:')
 
-#serverHOST = '127.0.0.1'
-serverPORT = 5000
+extensionFile = open('servConfig/extension.txt','r')
+extensionDict = {}
+for line in extensionFile:
+    keyValue = line.split()
+    extensionDict[keyValue[1]] = keyValue[0]
+    
 
-#cria o socket
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if url.find('/') < 0:
+    url = url + '/'
+    
+indexOf = url.find('/')
+host = url[:indexOf] #endereço
+getOperation = url[indexOf:]
 
-try:
-    tcp.connect((serverHOST, serverPORT))
-    print(f'Conectado ao servidor..', serverHOST)
+fileExtension = ''
 
-    while True:
-        cmds = input("Cliente envia mensagem: ")
-        if cmds == 'q':
-            break
-            tcp.close()
-        sends = bytes(cmds, "utf-8")
-        tcp.send(sends)
-        data = tcp.recv(1024).decode("utf-8")
-        print("Servidor responde{}".format(data))
-        continue
+if(getOperation == '/'):
+    fileExtension = '.html'
 
-except socket.error as e:
-    print('Erro:', str(e))
-finally:
-    tcp.close()
+else:
+    indexOfPoint = getOperation.rfind('.')
+    if (indexOfPoint < 0):
+        fileExtension = '.html'
+    else:
+        fileExtension = getOperation[indexOfPoint:]
+
+operation = 'GET ' + getOperation+ ' HTTP/1.1\r\n'
+
+request = {
+    'Host: ' : host,
+    'Connection: ' : 'keep-alive',
+    'Upgrade-Insecure-Requests: ' : '1',
+    'User-Agent: ' : 'jaoClient',
+    'Acept-Encoding: ' : 'gzir, deflate, br',
+    'Accept-Language: ' : 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+}
 
 
+HOST = host.split(':')[0]
+
+try :
+    PORT = host.split(':')[1]
+except:
+    PORT = 80
+    
+addr = ((HOST,int(PORT)))
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IP-TCP
+client_socket.connect(addr)
+
+stringRequest = operation
+for k,v in request.items():
+    stringRequest = stringRequest+ k + v + '\n'
+
+client_socket.send(stringRequest.encode())
+
+
+total_data=[]
+while True:
+    data = client_socket.recv(8192)
+    if not data: break
+    total_data.append(data)
+response = b''.join(total_data)
+
+header = response.decode(errors="ignore")
+header = header[:header.rfind('\r\n\r\n')]
+
+headerSplit = header.split('\r\n')
+status = headerSplit[0]
+del(headerSplit[0])
+print(status)
+
+headerDict = {}
+for line in headerSplit:
+    keyValue = line.split(': ')
+    try:
+        headerDict[keyValue[0]] = keyValue[1]
+    except:
+        break
+
+saveBinFile = response[len(header)+4:]
+
+
+if getOperation == '/':
+    fileName= 'Index'
+else:
+    indexOfLBar = getOperation.rfind('/')
+    fileName = getOperation[indexOfLBar:indexOfPoint]
+    
+downloadPath = 'downloads/' + fileName + fileExtension
+
+fileName
+
+if status.split()[1] == '200':
+    download = None 
+    if headerDict['Content-Type'] != 'text/html':
+        download = open(downloadPath,'wb')
+        download.write(saveBinFile)
+        download.close()
+    else:
+        download = open(downloadPath,'w')
+        decodeFile = saveBinFile.decode()
+        download.write(decodeFile)
+        download.close()
+else:
+    print('erro ao processar a requisição do arquivo')
+   
 
